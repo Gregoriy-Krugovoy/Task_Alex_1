1f42596e2f4998827c4830bd31adb7d766c10144
Greee <no_signal@i.ua>
2024-03-13 16:36:57 +0200

    in progress

--- Alex_1.2.c
+++ Alex_1.2.c
@@ -3,18 +3,37 @@
 // от 0 до 255. Необходимо найти минимальное и максимальное значение 
 // массива, а также их позиции в массиве.
 
-#include <stdio.h> // ЗФСБ С ввода вывода
+#include <stdio.h>
+#include <stdint.h>
 
-int main ( void ) {
+#define ARRAY_SIZE 512
 
-    int element = 512;
-    int value 255;
-    int t; // счетчик
+int main() {
+    uint8_t array[ARRAY_SIZE];
+    uint8_t arr[] = { 0 };
+    uint8_t min_value = array[0];
+    uint8_t max_value = array[0];
+    int min_index = 0;
+    int max_index = 0;
+    int i;
 
-    for( t = 0; t < 512; ++t ) {
-        
+    // Находим минимальное и максимальное значение, а также их позиции в массиве
+    for ( i = 1; i < ARRAY_SIZE; ++i) {
+        if ( array[ i ] <= min_value ) {
+            min_value = array[i];
+            min_index = i;
+            ++arr[ array[ min_value ] ]; 
+        }
+        if ( array[ i ] >= max_value ) {
+            max_value = array[i];
+            max_index = i;
+            ++arr[ array[ max_value ] ];
+        }
     }
-    printf( "Enter the number of elements: " );
-    scanf( "%d/n", &element );
-}
 
+    // Выводим результаты
+    printf("Минимальное значение: %d, расположение в массиве: %d\n", min_value, min_index);
+    printf("Максимальное значение: %d, расположение в массиве: %d\n", max_value, max_index);
+
+    return 0;
+}
